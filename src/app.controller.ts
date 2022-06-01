2ee125be365982e262c6273290b4efb4553c1979
sebastian roa soraca <sroasoraca@outlook.com>
2022-06-01 16:43:06 -0500

    Recibir parametros

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
@@ -13,12 +13,25 @@ export class AppController {
   //Creando nuevos End Points
   //http://localhost:3000/nuevo
   @Get('nuevo')
-  newEndPoint(){
+  newEndPoint() {
     return "Yo soy nuevo";
   }
 
   @Get('/ruta')
-  hello(){
+  hello() {
     return "nueva ruta";
+  }
+
+  //recibir Paramteros
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `Product ${productId}`;
+  }
+
+  @Get('categories/:categoriesId/product/:productId')
+  getCategories(
+    @Param('categoriesId') categoriesId: string,
+    @Param('productId') productId: string) {
+    return `Categories ${categoriesId} and productId ${productId}`;
   }
 }
