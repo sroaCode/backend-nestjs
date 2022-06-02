8816e8b65e7c6db312ee21cced45a925e3cd4246
sebastian roa soraca <sroasoraca@outlook.com>
2022-06-02 00:21:19 -0500

    Se crean Controllers de brands, customer, orders y users (solo platillas)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesController } from './controllers/categories/categories.controller';
 import { ProductsController } from './controllers/products/products.controller';
+import { BrandsController } from './controllers/brands/brands.controller';
+import { UsersController } from './controllers/users/users.controller';
+import { CostumersController } from './controllers/costumers/costumers.controller';
+import { OrdersController } from './controllers/orders/orders.controller';
+import { CustumerController } from './controllers/custumer/custumer.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CategoriesController, ProductsController],
+  controllers: [AppController, CategoriesController, ProductsController, BrandsController, UsersController, CostumersController, OrdersController, CustumerController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/controllers/custumer/custumer.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('custumer')
+export class CustumerController {}
